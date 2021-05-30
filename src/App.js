2d335b5121a74de5cf1a013ce97fd764a6d289bc
Waiyakorn Phoomjarownwat <waiyakorn.krow@gmail.com>
2021-05-30 23:19:36 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Header from './Header'
+import SearchInput from './SearchInput'
+import EmojiResults from './EmojiResults'
+import filterEmoji from './filterEmoji'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredEmoji: filterEmoji("",20)     
+    }
+    
+    //case 1 เขียนใน constructor
+    this.handleSearchChange= (event)=>{
+      this.setState({
+        filteredEmoji: filterEmoji(event.target.value,20)     
+      })
+    }
+
+    // this.handleSearchChange = this.handleSearchChange.bind(this); //อันนี้คือ bind case 2
+  }
+
+  /*
+  case 2 ต้อง bind
+  handleSearchChange(event){
+    this.setState({
+      filteredEmoji: filterEmoji(event.target.value,20)     
+    })
+  }
+  case 3 arrow ไม่ต้อง bind
+  handleSearchChange= (event)=>{
+    this.setState({
+      filteredEmoji: filterEmoji(event.target.value,20)     
+    })
+  }*/
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <SearchInput textChange={this.handleSearchChange} />
+        <EmojiResults emojiData={this.state.filteredEmoji}/> {/* 1.ส่ง props emojiData ไปที่ EmojiResults */}
+      </div>
+    )
+  }
 }
-
-export default App;
